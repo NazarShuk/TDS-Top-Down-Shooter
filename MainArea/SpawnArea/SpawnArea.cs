36639d0adbc79d6e2393e2042ed2651346e9ce43
alexandros3015 <alexandrosbeachtoys@gmail.com>
2025-11-02 16:40:09 -0500

    Add new images and update Godot editor states
    Added skel.png and title67.png assets to Assets/Image and updated their import files. Modified Godot editor configuration files to reflect new node selections, folding states, and recent directories. Updated scene and script metadata, as well as project and editor layout settings to accommodate new assets and workflow changes.

--- MainArea/SpawnArea/SpawnArea.cs
+++ MainArea/SpawnArea/SpawnArea.cs
@@ -1,11 +1,17 @@
 using System;
 using Godot;
-// Range: 642 px
+// Range: 340 px
 
 namespace tdstopdownshooter.MainArea.SpawnArea;
 
 public partial class SpawnArea : Node2D
 {
+    [Export]
+    public float SpawnMaxRange = 340f;
+    
+    [Export]
+    public float SpawnMinRange = 50f;
+    
     private Timer _timer;
     private RandomNumberGenerator _rng;
 
@@ -22,7 +28,7 @@ public partial class SpawnArea : Node2D
         var enemyScene = ResourceLoader.Load<PackedScene>("res://Enemy/EnemyBasic/EnemyBasic.tscn");
         var enemy = enemyScene.Instantiate<Node2D>();
         AddSibling(enemy);
-        enemy.Position = new Vector2(0f, _rng.RandfRange(30, 642));
+        enemy.Position = new Vector2(0f, _rng.RandfRange(SpawnMinRange, SpawnMaxRange));
 
         _timer.WaitTime = MathF.Max(0.5f, Global.Global.Difficulty * -0.2f + 10f);
     }

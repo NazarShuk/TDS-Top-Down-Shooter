7a6fb08f8e9f76f3545cf55789d169d0033068e5
NshIS <nazarshuxardinissuper@gmail.com>
2025-11-11 20:29:24 -0500

    camera shake when damaged

---
+++ MainArea/Zone/Camera2d.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class Camera2d : Camera2D
+{
+    [Export]
+    private Shaker _shaker;
+
+    private int _previousHealth = Global.Health;
+    
+    public override void _Process(double delta)
+    {
+        if (_previousHealth != Global.Health)
+        {
+            _previousHealth = Global.Health;
+            _shaker.Shake();
+            GD.Print("health changed");
+        }
+    }
+}
